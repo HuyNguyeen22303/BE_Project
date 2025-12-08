e5eaab5b14cd4bfd6c288c661febeb7999b1213c
Nguyễn Dương Hoàng Huy <Huynguyeen22303@gmail.com>
2025-12-08 23:57:20 +0700

    Fix: Xoa thu muc node_modules nang

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 
 const route = require("./routes/client/index.route");
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-
 
 
 
