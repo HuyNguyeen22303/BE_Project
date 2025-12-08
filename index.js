df3c26229bc92bebb022f550419a1d91a968d5e4
Nguyễn Dương Hoàng Huy <Huynguyeen22303@gmail.com>
2025-12-08 23:25:26 +0700

    Khoi tao du an

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const route = require("./routes/client/index.route");
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+
+
+
+route(app);
+
+
+
+app.listen(port, () => {
+
+    console.log(`Example app listening on port ${port}`)
+
+
+})
