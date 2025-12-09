f0fb769e537889b61b2dfc613dd67f1ab1feec61
Nguyễn Dương Hoàng Huy <Huynguyeen22303@gmail.com>
2025-12-09 17:09:21 +0700

    cau hinh mongoDB

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT;
-
+const mongoose = require('mongoose');
 const route = require("./routes/client/index.route");
 
+mongoose.connect("mongodb://localhost:27017/");
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
